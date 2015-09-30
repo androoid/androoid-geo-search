0e487934aec68226f098ed9a9b4180c13c732115
Juan Carlos García <jugade92@gmail.com>
2015-09-30 23:21:34 +0200

    Initial version of AndrooidGeoSearch library

---
+++ src/main/java/io/androoid/geo/search/AddressSearchHelper.java
@@ -0,0 +1,57 @@
+package io.androoid.geo.search;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.osmdroid.util.GeoPoint;
+
+import android.os.AsyncTask;
+import fr.dudie.nominatim.client.JsonNominatimClient;
+import fr.dudie.nominatim.model.Address;
+
+/**
+ *
+ * This Utility class provides the necessary methods to obtain GeoPoint from an
+ * existing address.
+ *
+ * This class will be invoked by some Activity that wants to obtain a GeoPoint
+ * using a provided address.
+ *
+ * Uses Nominatim Java API to get GeoPoints from an existing address.
+ *
+ * @author Juan Carlos García
+ * @since 1.0
+ */
+public class AddressSearchHelper extends AsyncTask<String, Integer, GeoPoint> {
+
+	public AsyncGeoResponse delegate = null;
+	private JsonNominatimClient nominatiumClient = new JsonNominatimClient(new DefaultHttpClient(),
+			"your-email@androoid.io");
+
+	/**
+	 * Method that returns GEO information about some address
+	 *
+	 * @param address
+	 * @return
+	 */
+	@Override
+	protected GeoPoint doInBackground(String... address) {
+		try {
+			List<Address> result = nominatiumClient.search(address[0]);
+
+			for (Address resultAddress : result) {
+				return new GeoPoint(resultAddress.getLatitude(), resultAddress.getLongitude());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	@Override
+	protected void onPostExecute(GeoPoint result) {
+		delegate.processFinish(result);
+	}
+}
---
+++ src/main/java/io/androoid/geo/search/AsyncGeoResponse.java
@@ -0,0 +1,16 @@
+package io.androoid.geo.search;
+
+import org.osmdroid.util.GeoPoint;
+
+/**
+ * @author Juan Carlos García
+ * @since 1.0
+ */
+public interface AsyncGeoResponse {
+
+    /**
+     * Method that will be executed when async task finishes
+     * @param output
+     */
+    void processFinish(GeoPoint output);
+}
